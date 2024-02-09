2e169a0d034a2a6aa93d7708e60f05c8ff1d0bfc
eelanpy <76450488+eelanpy@users.noreply.github.com>
2024-02-09 08:37:44 -0500

    Delete src/getAllSymbols.py

--- src/getAllSymbols.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-json_data = []
-with open('/Users/eelank/Documents/react/reactmega/src/stock_list.json') as f:
-  data = json.load(f)["data"]["table"]
-  json_data = [i["symbol"].replace("^", "") for i in data["rows"]]
-with open('/Users/eelank/Documents/react/reactmega/src/stock_list.json', 'w') as f:
-    json.dump(json_data, f, indent=2)
